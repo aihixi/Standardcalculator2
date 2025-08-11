6f49cb580c1a93bd361070d493c85fc83688cd4f
aihixi <162949415+aihixi@users.noreply.github.com>
2025-08-11 18:29:43 +0800

    web部分修改

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -7,7 +7,7 @@ import TitleBar from './components/Mytitle'
 function App(): React.JSX.Element {
   return (
     <div>
-      <TitleBar />
+      {window.api && <TitleBar />}
       <RouterConfig />
     </div>
   )
